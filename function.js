6b654e27db91b3a8496b56905cc5fe096341174e
ISE <ise.sns46@gmail.com>
2024-10-04 14:52:47 +0700

    Add more item and carousel

--- function.js
+++ function.js
@@ -18,5 +18,5 @@ function updateCart() {
 function checkout() {
     let cartSummary = cart.map(item => `Product ${item.productId}: ${item.quantity} pcs`).join('\n');
     let totalPrice = cart.reduce((sum, item) => sum + (item.price * item.quantity), 0);
-    alert(`Cart Summary:\n${cartSummary}\nTotal Price: $${totalPrice.toFixed(2)}`);
+    alert(`Cart Summary:\n${cartSummary}\nTotal Price: ${totalPrice.toFixed(2)} Baht`);
 }
